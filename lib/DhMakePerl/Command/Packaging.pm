5b720ea9fcf5760ca0b83c1ad4b078f199efd6d7
Salvatore Bonaccorso <carnil@debian.org>
2016-02-02 06:44:22 +0100

    Create packages using Standards-Version 3.9.7

--- lib/DhMakePerl/Command/Packaging.pm
+++ lib/DhMakePerl/Command/Packaging.pm
@@ -45,7 +45,7 @@ use Text::Balanced qw(extract_quotelike);
 use Text::Wrap qw(fill);
 use User::pwent;
 
-use constant debstdversion => '3.9.6';
+use constant debstdversion => '3.9.7';
 
 our %DEFAULTS = (
 );
