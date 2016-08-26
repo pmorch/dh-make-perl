c2e4353cd5a0c714c241b666b4fadfdc0b817fe5
gregor herrmann <gregoa@debian.org>
2016-08-27 00:15:31 +0200

    update warning about 'apt-file' to newer version
    Gbp-Dch: Ignore

--- lib/DhMakePerl/Command/Packaging.pm
+++ lib/DhMakePerl/Command/Packaging.pm
@@ -1429,7 +1429,7 @@ sub discover_dependencies {
     }
     else {
         warn "No APT contents can be loaded.\n";
-        warn "Please install 'apt-file' package (at least version 2.5.0) and\n";
+        warn "Please install the 'apt-file' package (at least version 3) and\n";
         warn "run 'apt-file update' as root.\n";
         warn "Dependencies not updated.\n";
 
